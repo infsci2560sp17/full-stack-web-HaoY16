7ad3765242d0be5915a693f5ca587ca8f222763f
HaoY16 <hay46@pitt.edu>
2017-03-25 22:46:27 -0400

    Update BlogsController.java

--- src/main/java/edu/infsci2560/controllers/BlogsController.java
+++ src/main/java/edu/infsci2560/controllers/BlogsController.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,13 +10,9 @@ import edu.infsci2560.models.Blog;
 import edu.infsci2560.repositories.BlogRepository;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,4 +38,3 @@ public class BlogsController {
     }
     
 }
-
